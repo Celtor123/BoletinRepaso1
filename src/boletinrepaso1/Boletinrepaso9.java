5c851d3c68f91e6e103d72f367b64b63426f7229
Juan <Juan@DESKTOP-J5PU6RU>
2018-02-04 16:42:26 +0100

    Avanzando 2.

--- src/boletinrepaso1/Boletinrepaso9.java
+++ src/boletinrepaso1/Boletinrepaso9.java
@@ -3,5 +3,11 @@ package boletinrepaso1;
 
 
 public class Boletinrepaso9 {
+    public static void main(String[] args) {
+        
+    }
     
 }
+
+//9- Sumar unha serie de números que pedimos por teclado . 
+//Cando a suma sexa 100 ou superior visualizamos o resultado .
