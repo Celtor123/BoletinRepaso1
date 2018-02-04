2e8923ee3daa6d89cdf87ce14b4d4e2ece917d8b
Juan <Juan@DESKTOP-J5PU6RU>
2018-02-04 17:01:59 +0100

    Avanzando 3.

--- src/boletinrepaso1/Boletinrepaso11.java
+++ src/boletinrepaso1/Boletinrepaso11.java
@@ -1,9 +1,25 @@
 
 package boletinrepaso1;
 
+import javax.swing.JOptionPane;
+
 public class Boletinrepaso11 {
+    public void alumnos(){
+    int r=0;
+        while(r<6){
+       String a= JOptionPane.showInputDialog("inserte a nota do módulo: ");
+       int e = Integer.parseInt(a);
+       r++;
+    }
+       
+       
+       
+       JOptionPane.showMessageDialog(null, e);
+    }
     public static void main(String[] args) {
         
     }
     
 }
+
+//11 – Supon que nunha clase están 3 alumnos . Calcula a nota media para cada un dos alumnos .
